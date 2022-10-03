d59bb526b9d6983ff361dc68dbfc12a05ba96995
Pau Salanova Blanco <pauete.sb@gmail.com>
2022-10-03 15:35:15 +0200

    Test command now starts a game with arguments

--- src/main/kotlin/com/fadedbytes/MCBattleship/command/FastTestCommand.kt
+++ src/main/kotlin/com/fadedbytes/MCBattleship/command/FastTestCommand.kt
@@ -1,6 +1,7 @@
 package com.fadedbytes.MCBattleship.command
 
-import com.fadedbytes.MCBattleship.mcgame.features.canon.FroglightCanon
+import com.fadedbytes.MCBattleship.mcgame.MinecraftGame
+import org.bukkit.ChatColor
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
@@ -9,7 +10,7 @@ import org.bukkit.entity.Player
 class FastTestCommand: CommandExecutor {
 
     companion object {
-        var canon: FroglightCanon? = null
+
     }
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
@@ -18,11 +19,23 @@ class FastTestCommand: CommandExecutor {
             return true
         }
 
-        if (canon == null) {
-            canon = FroglightCanon(sender.location)
-        } else {
-            canon?.prepareShoot(sender.location)
+        try {
+            val radar = sender.location.world?.getBlockAt(args[0].toInt(), args[1].toInt(), args[2].toInt()) !!
+            val canon = sender.location.world?.getBlockAt(args[3].toInt(), args[4].toInt(), args[5].toInt()) !!
+            val shipboard = sender.location.world?.getBlockAt(args[6].toInt(), args[7].toInt(), args[8].toInt()) !!
+
+            val game = MinecraftGame(
+                sender,
+                10,
+                radar,
+                canon,
+                shipboard
+            )
+        } catch (e: Exception) {
+            sender.sendMessage("${ChatColor.RED}Invalid arguments")
         }
+
+
 
         return true
     }
