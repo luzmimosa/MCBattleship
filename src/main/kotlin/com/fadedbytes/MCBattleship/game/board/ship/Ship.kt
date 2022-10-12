1fc5ff9f8dfd52f64956a73edd061a81a3896682
Pau Salanova Blanco <pauete.sb@gmail.com>
2022-10-12 23:39:48 +0200

    Ship list fixed

--- src/main/kotlin/com/fadedbytes/MCBattleship/game/board/ship/Ship.kt
+++ src/main/kotlin/com/fadedbytes/MCBattleship/game/board/ship/Ship.kt
@@ -1,10 +1,9 @@
 package com.fadedbytes.MCBattleship.game.board.ship
 
 enum class Ship(val size: Int) {
-//    CARRIER(5),
-//    BATTLESHIP(4),
-//    CRUISER(3),
-//    SUBMARINE(3),
-//    DESTROYER(2);
-    UWU(10);
+    CARRIER(5),
+    BATTLESHIP(4),
+    CRUISER(3),
+    SUBMARINE(3),
+    DESTROYER(2);
 }
